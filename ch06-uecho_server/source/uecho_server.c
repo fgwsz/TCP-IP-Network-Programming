825986890a3a379c942291d36c187ea92c4b5678
fgwsz <3191000458@qq.com>
2025-01-01 15:08:19 +0800

    impl ch06-uecho_server/client

--- ch06-uecho_server/source/uecho_server.c
+++ ch06-uecho_server/source/uecho_server.c
@@ -5,39 +5,36 @@
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
+#define BUF_SIZE 30
 void error_handling(char const* message);
 int main(int argc,char* argv[]){
     int serv_sock;
-    int clnt_sock;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size;
-    char message[]="Hello World!";
+    char message[BUF_SIZE];
+    int str_len;
+    socklen_t clnt_adr_sz;
+    struct sockaddr_in serv_adr,clnt_adr;
     if(argc!=2){
         printf("Usage:%s <port>\n",argv[0]);
         exit(1);
     }
-    serv_sock=socket(PF_INET,SOCK_STREAM,0);
+    serv_sock=socket(PF_INET,SOCK_DGRAM,0);
     if(serv_sock==-1){
-        error_handling("socket() error!");
+        error_handling("UDP socker creation error!");
     }
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atoi(argv[1]));
-    if(bind(serv_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1){
+    memset(&serv_adr,0,sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_adr.sin_port=htons(atoi(argv[1]));
+    if(bind(serv_sock,(struct sockaddr*)&serv_adr,sizeof(serv_adr))==-1){
         error_handling("bind() error!");
     }
-    if(listen(serv_sock,5)==-1){
-        error_handling("listen() error!");
+    while(1){
+        clnt_adr_sz=sizeof(clnt_adr);
+        str_len=recvfrom(serv_sock,message,BUF_SIZE,0,
+            (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+        sendto(serv_sock,message,str_len,0,
+            (struct sockaddr*)&clnt_adr,clnt_adr_sz);
     }
-    clnt_addr_size=sizeof(clnt_addr);
-    clnt_sock=accept(serv_sock,(struct sockaddr*)&clnt_addr,&clnt_addr_size);
-    if(clnt_sock==-1){
-        error_handling("accept() error!");
-    }
-    write(clnt_sock,message,sizeof(message));
-    close(clnt_sock);
     close(serv_sock);
     return 0;
 }
