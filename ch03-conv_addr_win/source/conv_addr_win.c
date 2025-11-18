8926a65c38cbb35e88c4e8a6b3b5d9f6c11391d9
fgwsz <3191000458@qq.com>
2025-11-18 10:22:14 +0800

    增加了第三章的一部分内容

---
+++ ch03-conv_addr_win/source/conv_addr_win.c
@@ -0,0 +1,37 @@
+/*
+#undef用于取消之前定义的宏.
+根据项目环境,VC++会自主声明这2个宏,
+在之后调用的转换函数中,参数就将转换为unicode形式,给出错误的运行结果.
+所以插入了这2句宏定义
+*/
+#undef UNICODE
+#undef _UNICODE
+#include<stdio.h>
+#include<winsock2.h>
+
+int main(int argc,char* argv[]){
+
+    char* strAddr="203.211.218.102:9190";
+    char strAddrBuf[50];
+    SOCKADDR_IN servAddr;
+    int size;
+
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,2),&wsaData);
+
+    size=sizeof(servAddr);
+    // 调用WSAStringToAddress函数转换成结构体
+    WSAStingToAddress(
+        strAddr,AF_INET,NULL,(SOCKADDR*)&servAddr,&size
+    );
+
+    size=sizeof(strAddrBuf);
+    // 调用WSAAddressToString函数将结构体转换为字符串
+    WSAAddressToString(
+        (SOCKADDR*)&servAddr,sizeof(servAddr),NULL,strAddrBuf,&size
+    );
+
+    printf("Second conv result: %s \n",strAddrBuf);
+    WSACleanup();
+    return 0;
+}
